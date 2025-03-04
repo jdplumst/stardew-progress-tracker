60be19d4674506a429149cb3f63dc315c30c99f0
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-03-03 23:29:59 -0500

    Add invite user mutation

--- src/server/actions/farms.ts
+++ src/server/actions/farms.ts
@@ -2,7 +2,7 @@
 
 import { z } from "zod";
 import { isAuthed } from "~/server/db/queries/auth";
-import { createFarm } from "~/server/mutations/farms";
+import { createFarm, inviteUser } from "~/server/mutations/farms";
 import { type ErrorResponse, type MessageResponse } from "~/lib/types";
 
 export async function createFarmAction(
@@ -16,16 +16,37 @@ export async function createFarmAction(
     map: z.coerce.number().min(1),
   });
 
-  console.log("Form data:", formData);
-
   const input = formSchema.safeParse(Object.fromEntries(formData));
   if (input.error) {
     return {
       success: false,
       error:
-        "You must enter a farm name between 1 and 30 characters and you must select a farm map.",
+        "You must enter a farm name between 1 and 30 characters and you must select a farm map",
     };
   }
 
-  return await createFarm({ ...input.data, userId: auth.user.id });
+  return await createFarm(input.data.name, input.data.map, auth.user.id);
+}
+
+export async function inviteUserAction(
+  _previousState: unknown,
+  formData: FormData,
+): Promise<MessageResponse | ErrorResponse> {
+  const auth = await isAuthed();
+
+  const formSchema = z.object({
+    username: z.string().min(1),
+    farmId: z.string().min(1),
+  });
+
+  const input = formSchema.safeParse(Object.fromEntries(formData));
+  if (input.error) {
+    console.log("Input error:", input.error);
+    return {
+      success: false,
+      error: "You must enter a username",
+    };
+  }
+
+  return await inviteUser(input.data.username, input.data.farmId, auth.user.id);
 }
--- src/server/mutations/farms.ts
+++ src/server/mutations/farms.ts
@@ -1,45 +1,79 @@
 import "server-only";
 
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "~/server/db";
-import { farm, farmMap, farmUser } from "~/server/db/schema";
+import { farm, farmMap, farmUser, user } from "~/server/db/schema";
 import { type ErrorResponse, type MessageResponse } from "~/lib/types";
 import { revalidatePath } from "next/cache";
-export async function createFarm(data: {
-  name: string;
-  map: number;
-  userId: string;
-}): Promise<MessageResponse | ErrorResponse> {
-  const validMap = await db
-    .select()
-    .from(farmMap)
-    .where(eq(farmMap.id, data.map));
+import { redirect } from "next/navigation";
+export async function createFarm(
+  name: string,
+  map: number,
+  userId: string,
+): Promise<MessageResponse | ErrorResponse> {
+  const validMap = await db.select().from(farmMap).where(eq(farmMap.id, map));
   if (!validMap) {
-    return { success: false, error: "You must select a farm map." };
+    return { success: false, error: "You must select a farm map" };
   }
 
   const farmData = (
     await db
       .insert(farm)
       .values({
-        name: data.name,
-        farmMapId: data.map,
+        name: name,
+        farmMapId: map,
         updatedAt: new Date(),
       })
       .returning()
   )[0];
 
   if (!farmData) {
-    return { success: false, error: "Something went wrong. Please try again." };
+    return { success: false, error: "Something went wrong. Please try again" };
   }
 
   await db.insert(farmUser).values({
     farmId: farmData.id,
-    userId: data.userId,
+    userId: userId,
     pending: false,
   });
 
   revalidatePath("/farms");
 
-  return { success: true, message: "Farm created successfully." };
+  return { success: true, message: "Farm created successfully" };
+}
+
+export async function inviteUser(
+  username: string,
+  farmId: string,
+  userId: string,
+): Promise<MessageResponse | ErrorResponse> {
+  const farmData = await db.select().from(farm).where(eq(farm.id, farmId));
+  if (!farmData) {
+    redirect("/farms");
+  }
+
+  const farmUserData = await db
+    .select()
+    .from(farmUser)
+    .where(and(eq(farmUser.userId, userId), eq(farmUser.farmId, farmId)));
+
+  if (!farmUserData) {
+    redirect("/farms");
+  }
+
+  const invitedUser = (
+    await db.select().from(user).where(eq(user.name, username))
+  )[0];
+
+  if (!invitedUser) {
+    return { success: false, error: "User not found" };
+  }
+
+  await db.insert(farmUser).values({
+    farmId: farmId,
+    userId: invitedUser.id,
+    pending: true,
+  });
+
+  return { success: true, message: "User invited successfully" };
 }
