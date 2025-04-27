8a9c0495bd6bb3ea6b43a907006858baba18b20c
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-04-27 17:11:32 -0400

    Return time from pretty-date

--- src/lib/pretty-date.ts
+++ src/lib/pretty-date.ts
@@ -13,10 +13,14 @@ export function prettyDate(date: Date) {
     "November",
     "December",
   ];
-  const monthIdx = date.getUTCMonth();
+  const monthIdx = date.getMonth();
   const month = months[monthIdx];
-  const day = date.getUTCDate();
-  const year = date.getUTCFullYear();
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formatedMinutes =
+    minutes > 10 ? minutes : minutes > 1 ? minutes + 10 : minutes;
 
-  return `${month} ${day}, ${year}`;
+  return `${month} ${day}, ${year} at ${hours % 12}:${minutes.toString().length >= 2 ? minutes : "0" + minutes}${hours >= 12 ? "pm" : "am"}`;
 }
--- src/tests/pretty-date.spec.ts
+++ src/tests/pretty-date.spec.ts
@@ -4,11 +4,11 @@ import { describe, expect, it } from "vitest";
 describe.concurrent("pretty date tests", () => {
   it("date with timestamp", () => {
     expect(prettyDate(new Date("2025-04-27T10:10:10.000z"))).toBe(
-      "April 27, 2025",
+      "April 27, 2025 at 6:10am", // converts to local time
     );
   });
 
   it("date without timestamp", () => {
-    expect(prettyDate(new Date("2025-04-27"))).toBe("April 27, 2025");
+    expect(prettyDate(new Date("2025-04-27"))).toBe("April 26, 2025 at 8:00pm");
   });
 });
