403e375d40025b0433bbf96405a127f329092b5a
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2025-04-28 00:32:46 -0400

    Configure vitest timezone in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     exclude: ["e2e", "node_modules"],
+    env: {
+      TZ: "EST",
+    },
   },
   resolve: {
     alias: {
